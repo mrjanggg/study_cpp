013b941e32a35f148d7cef53b37fdf414e892445
mrjanggg <ijin1322@gmail.com>
2026-04-20 23:51:39 +0900

    cpp 8일차

--- YSW_CPP/chapter_03/RacingCarFuncAdd.cpp
+++ YSW_CPP/chapter_03/RacingCarFuncAdd.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 using namespace std;
 
-#define ID_LEN      20
-#define MAX_SPD     200
-#define FUEL_STEP   2
-#define ACC_STEP    10
-#define BRK_STEP    10
-
 struct Car
 {
+    enum
+    {
+        ID_LEN      = 20,
+        MAX_SPD     = 200,
+        FUEL_STEP   = 2,
+        ACC_STEP    = 10,
+        BRK_STEP    = 10
+    };
+
     char gamerID[ID_LEN];       //소유자 ID
     int fuelGauge;              //연료량
     int curSpeed;               //현재속도
